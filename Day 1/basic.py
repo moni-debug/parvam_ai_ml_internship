e9263893f4513c339ca028c9460a7f5bc95816fb
moni-debug <velmurugaamoni@gmail.com>
2025-06-26 15:12:11 +0530

    Python basics: List, Tuple, Dictionary

---
+++ Day 1/basic.py
@@ -0,0 +1,67 @@
+greet = "hello world"
+print(greet)
+print(type(greet))
+num = 10
+print("the value of num is:",num)
+print(type(num))
+num1 = 10.5
+print("the value of num1:",num1)
+print(num1)
+print(type(num1))
+var = True
+print(var)
+print(type(var))
+comp = 2j
+print(comp)
+print(type(comp))
+fruits = ["apple","orange","greaps","gova"]
+print(fruits)
+print(type(fruits))
+print(fruits[3])
+print(len(fruits))
+if "gova" in fruits:
+    print("yes")
+else:
+    print("no")
+list = [2,4,7,8]    
+print(list)
+list.append(10)
+print(list)
+list.append(15)
+print(list)
+list.append(20)
+print(list)
+list.insert(2,25)
+print(list)
+print(list[-2])
+print(list[2:7])
+print(list[:7])
+print(list[4:])
+print(list[-1:])
+if 30 in list:
+    print("yes")
+else:
+    print("no")
+list.extend(list)  
+print(list)
+list.remove(20)
+print(list)
+print("popping")
+list.pop()
+list.pop(10)
+print(list)
+list.clear()
+print(list)
+list1 = [10,80,30,100,50,20]
+list1.sort()
+print(list1)
+
+
+
+
+
+
+
+    
+
+
